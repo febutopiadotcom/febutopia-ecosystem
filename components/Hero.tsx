ab82938950122439b919536564d57d1262ed2a52
febutopiadotcom <vrincvi@gmail.com>
2026-07-14 13:54:25 +0700

    Create Hero.tsx

---
+++ components/Hero.tsx
@@ -0,0 +1,46 @@
+export default function Hero(){
+
+return(
+
+<section className="
+min-h-screen
+flex
+flex-col
+justify-center
+items-center
+bg-gradient-to-br
+from-blue-600
+to-purple-700
+text-white
+">
+
+
+<h1 className="
+text-7xl
+font-bold
+">
+
+🌎 Febutopia
+
+</h1>
+
+
+<p className="
+text-xl
+mt-6
+max-w-xl
+text-center
+">
+
+A fictional utopian society where
+equality, justice, cooperation,
+and human well-being define civilization.
+
+</p>
+
+
+</section>
+
+)
+
+}
