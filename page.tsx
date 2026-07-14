4edfacc9dfc3614216b1a8d973f5e3160bbda44d
febutopiadotcom <vrincvi@gmail.com>
2026-07-14 13:54:04 +0700

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,25 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import GovernanceOverview from "@/components/GovernanceOverview";
+import ProposalBoard from "@/components/ProposalBoard";
+
+
+export default function Home(){
+
+return (
+
+<main>
+
+<Hero/>
+
+<Features/>
+
+<GovernanceOverview/>
+
+<ProposalBoard/>
+
+</main>
+
+)
+
+}
